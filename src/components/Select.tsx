820d47e03f8e86f8bdbc53d7d38bcbcea705d6c2
Francisco Pedroza <franciscofabriciopm@gmail.com>
2024-01-08 12:30:28 -0600

    hola

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -18,8 +18,11 @@ const Select: FC<iSelectProps> = ({ changeData, setIsSelected }) => {
     }
 
     return (
-        <div className="select-api-div">
-            <label htmlFor="selectedApi">Please Select an API</label>
+        <div className="bg-green-400 flex flex-col items-center">
+            <label htmlFor="selectedApi" className="text-2xl font-bold font-sans">
+                Please Select an API
+            </label>
+            <h1>HOLA</h1>
             <input type="url" placeholder={"https://jsonplaceholder.typicode.com/users"} id={"selectedApi"} ref={selectRef} onKeyUp={(e) => {
                 if (e.key === "Enter") {
                     clickHandler()
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
-    content: ["./src/**/*.tsx",
+    content: ["./src/**/*.{tsx, ts, html}",
         "./public/*.html"],
     theme: {
         extend: {
